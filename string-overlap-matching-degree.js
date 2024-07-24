2b9716f2494dd4d4a8d27c3477738a3ef34dd3bc
TisHub <121622972+My-Search@users.noreply.github.com>
2024-07-24 10:07:21 +0800

    Update string-overlap-matching-degree.js

--- string-overlap-matching-degree.js
+++ string-overlap-matching-degree.js
@@ -30,7 +30,6 @@ function overlapMatchingDegree(keyword, topicWeighs = {}, sort = "desc") {
         }
         topicWeighs = _temp;
     }
-    debugger
     let topicList = Object.keys(topicWeighs)
     // topic map 得分
     const topicScores = topicList.map(topic => {
